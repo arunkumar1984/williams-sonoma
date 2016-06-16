0e266063e15097ee0e5177180cc6cc997dbd9280
Arunkumar Venkatraman <arunkumar.venkatraman@gmail.com>
2016-06-15 19:28:57 -0700

    Add class to specifie the supported file formats

---
+++ src/main/java/com/sqa/av/helpers/data/TextFormat.java
@@ -0,0 +1,27 @@
+/**
+ *   File Name: TextFormat.java<br>
+ *
+ *   Venkatraman, Arunkumar<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: Jun 13, 2016
+ *   
+ */
+
+package com.sqa.av.helpers.data;
+
+/**
+ * TextFormat //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author Venkatraman, Arunkumar
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public enum TextFormat {
+	CSV, JSON, TAB, XML
+}
